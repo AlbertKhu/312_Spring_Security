ba82167f43fa9b1181c5b1363bc900040d30fe78
AlbertKhu <x_albert@mail.ru>
2025-02-24 22:50:47 +0300

    update

---
+++ src/main/java/ru/kata/spring/boot_security/demo/services/UserDetailsServiceImpl.java
@@ -0,0 +1,38 @@
+package ru.kata.spring.boot_security.demo.services;
+
+import ru.kata.spring.boot_security.demo.models.User;
+import ru.kata.spring.boot_security.demo.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+
+        // Преобразуем роли в массив строк, убирая префикс "ROLE_"
+        String[] roles = user.getRoles().stream()
+                .map(role -> role.getName().replace("ROLE_", "")) // Убираем префикс "ROLE_"
+                .toArray(String[]::new);
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .roles(roles) // Передаем роли
+                .build();
+    }
+}
