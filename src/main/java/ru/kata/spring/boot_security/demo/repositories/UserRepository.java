8eb120609abde60e23cecc4d76458c886e16c769
AlbertKhu <x_albert@mail.ru>
2025-02-28 09:38:25 +0300

    update

--- src/main/java/ru/kata/spring/boot_security/demo/repositories/UserRepository.java
+++ src/main/java/ru/kata/spring/boot_security/demo/repositories/UserRepository.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Repository;
 import ru.kata.spring.boot_security.demo.models.User;
 import java.util.Optional;
 
-
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
